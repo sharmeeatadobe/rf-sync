fc44014653f9a95b486f48c99cc894bb6bf29ee6
Sharmee Biswas <sharmeeadobe@gmail.com>
2025-05-16 14:35:14 +0530

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # rf-sync
+POC to read session data from Rainfocus and perform the following tasks:
+- Read session template from DA
+- Create sessions page using the template from RF
+    check if any new session is created for an event -> use the master template with placeholders -> publish to AEM publish instance
+    if session is modified -> delete existing page -> use the master template with placeholders -> publish to AEM publish instance
+    if session is deleted -> delete existing page
+- Preview and publish to HLX
+
+
+# Setup
+
+1. npm i
+2. create a .env file
+```
+#RF
+API_PROFILE_ID=
+SESSION_ID=
+
+#DA
+DA_TOKEN=
+
+#HLX
+HLX_TOKEN=
+```
+3. node app.js
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mustacheExpress = require("mustache-express");
 const path = require("path");
 const { formattedDate } = require("./transformer");
 const RF = require("./services/rainfocus");
@@ -11,19 +10,23 @@ require("dotenv").config();
 const app = express();
 const PORT = 5001;
 
-// Register '.mustache' extension with Mustache Express
-app.engine("html", mustacheExpress());
-
-// Set 'views' directory for templates and set view engine
-app.set("views", path.join(__dirname, "templates"));
-app.set("view engine", "html");
-
 // Serve static files (optional)
 app.use(express.static(path.join(__dirname, "public")));
 
 // Route to render template
 app.get("/", async (req, res) => {
-  // Fetch data from Rainfocus
+  //Fetch session template from DA
+  const daUrl = "https://admin.da.live/source";
+  const daOrg = "adobecom";
+  const daPath = "/da-bacom/drafts/sharmeeb/t1-events/sample-template.html";
+  const daToken = process.env.DA_TOKEN;
+  const DAService = new DA(daUrl, daOrg, daPath, daToken);
+  const daHTML = await DAService.fetchDoc();
+  console.log("DA Document:", daHTML);
+  if (!daHTML) {
+    return res.status(500).send("Error fetching data from DA");
+  }
+  // Fetch data from Rainfocus for a single session
   const apiProfileId = process.env.API_PROFILE_ID;
   const sessionId = process.env.SESSION_ID;
   const RFService = new RF(apiProfileId, sessionId);
@@ -39,33 +42,21 @@ app.get("/", async (req, res) => {
     ...payload,
   };
 
-  //Fetch HTML from DA
-  const daUrl = "https://admin.da.live/source";
-  const daOrg = "adobecom";
-  const daPath = "/da-bacom/drafts/sharmeeb/t1-events/sample-template.html";
-  const daToken = process.env.DA_TOKEN;
-  const DAService = new DA(daUrl, daOrg, daPath, daToken);
-  const daHTML = await DAService.fetchDoc();
-  console.log("DA Document:", daHTML);
-  if (!daHTML) {
-    return res.status(500).send("Error fetching data from DA");
-  }
-
   // Render the template with the payload
   const dom = new JSDOM(daHTML);
   const updatedDOM = autoUpdateContent(dom.window.document, finalPayload);
 
   // TODO: Publish to EDS
-//   const hlxUrl = "https://admin.hlx.page/preview";
-//   const hlxOrg = "adobecom";
-//   const hlxPath = "/da-bacom/main/drafts/sharmeeb/t1-events/sample-event";
-//   const hlxToken = process.env.HLX_TOKEN;
-//   const HLXService = new HLX(hlxUrl, hlxOrg, hlxPath, hlxToken);
-//   const hlxRes = await HLXService.preview();
-//   console.log("HLX Document:", hlxHTML);
-//   if (!hlxHTML) {
-//     return res.status(500).send("Error fetching data from HLX");
-//   }
+  //   const hlxUrl = "https://admin.hlx.page/preview";
+  //   const hlxOrg = "adobecom";
+  //   const hlxPath = "/da-bacom/main/drafts/sharmeeb/t1-events/sample-event";
+  //   const hlxToken = process.env.HLX_TOKEN;
+  //   const HLXService = new HLX(hlxUrl, hlxOrg, hlxPath, hlxToken);
+  //   const hlxRes = await HLXService.preview();
+  //   console.log("HLX Document:", hlxHTML);
+  //   if (!hlxHTML) {
+  //     return res.status(500).send("Error fetching data from HLX");
+  //   }
 
   res.send(updatedDOM.documentElement.outerHTML);
 });
